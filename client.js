d60c525b99827e4efbc795716884f44ff2c6e455
Ramya Adimoolam <ramyaa1431@gmail.com>
2020-12-01 17:26:27 +0000

    Added player name to send to server upon connection

--- client.js
+++ client.js
@@ -6,6 +6,10 @@ const connect = function (){
     port: 50541
   });
   conn.setEncoding('utf8');
+  conn.on('connect',() =>{
+    console.log("Successfully connected to game server");
+    conn.write("Name:RUM");
+  })
   conn.on('data',(data) => {
     console.log("Server says: ",data);
   });
