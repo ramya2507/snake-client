4955f64b4deacf00f8e2bc98e8af0be1f135ab21
Ramya Adimoolam <ramyaa1431@gmail.com>
2020-12-01 17:04:18 +0000

    Refactored connect function into a separate module

--- play.js
+++ play.js
@@ -1,16 +1,4 @@
-const net = require('net');
-//Establishes connection with game server
-const connect = function (){
-  const conn = net.createConnection({
-    host: '10.0.2.15',
-    port: 50541
-  });
-  conn.setEncoding('utf8');
-  conn.on('data',(data) => {
-    console.log("Server says: ",data);
-  });
-  return conn;
-}
+const connect = require('./client');
 
 console.log("Connecting....");
 connect();
