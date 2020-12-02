ae5fecdad28fa4483391fa7da6f9ed7f94575315
Ramya Adimoolam <ramyaa1431@gmail.com>
2020-12-02 06:58:01 +0000

    Made it more user friendly

--- play.js
+++ play.js
@@ -2,5 +2,18 @@ const connect = require('./client');
 
 console.log("Connecting....");
 connect();
-
-
+const handlerUserInput = function(key) {
+  //to check ctrl+c
+  if(key === '\u0003'){
+    process.exit();
+  }
+}
+// SetUp User Interface
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data' , data => handlerUserInput(data));
+  return stdin;
+}
