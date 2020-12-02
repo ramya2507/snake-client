9ecaf69dc31f7320d9f479fdb355ede02b2b1a79
Ramya Adimoolam <ramyaa1431@gmail.com>
2020-12-02 07:05:06 +0000

    Refactored the code and added input function in input.js

--- play.js
+++ play.js
@@ -1,19 +1,6 @@
 const connect = require('./client');
-
+const setupInput = require('./input');
 console.log("Connecting....");
 connect();
-const handlerUserInput = function(key) {
-  //to check ctrl+c
-  if(key === '\u0003'){
-    process.exit();
-  }
-}
-// SetUp User Interface
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data' , data => handlerUserInput(data));
-  return stdin;
-}
+setupInput();
+
